48db2a9fdfde0ba0ee4b98ef82a4a5560ea15c4e
Ailak17 <104394423+Ailak17@users.noreply.github.com>
2022-08-18 13:56:11 -0300

    registro

--- IniciodeSesion.js
+++ IniciodeSesion.js
@@ -1,4 +1,20 @@
-/* if (localStorage.getItem("carrito")) {
-    carrito = JSON.parse(localStorage.getItem("carrito"));
-  
-} */
+function guardarlocal(){
+    var form = document.getElementById('forms').elements;
+    for(var i = 0; i<= form.length - 1; i++){
+        if(form[i].type == 'text',"email","pasword"){
+
+            console.log(form[i].value);
+            localStorage.setItem(form[i].id, JSON.stringify(form[i].value));
+        }
+    }
+    console.log(localStorage);
+}
+
+
+function cambiar() {
+    document.getElementById("forms").innerHTML = "<h1>Registro exitoso! Inicia sesion para continuar ";
+}
+document.getElementById("boton").onclick = function (){
+    guardarlocal()
+    cambiar();
+} ;
